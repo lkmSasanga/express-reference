edbf38f50b4a560deab6ef1b02ee54ea8e598e2f
lkmSasanga <lkmsasanga@gmail.com>
2021-01-21 09:48:15 +0530

    initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob William',
+        email: 'Bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jack',
+        email: 'Shannon@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
